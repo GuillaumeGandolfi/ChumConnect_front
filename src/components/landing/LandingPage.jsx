3aac4d38d2e12a1ee8ea801239d36f80dd82b880
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-09-19 00:35:32 +0200

    add content on the landing page

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -2,7 +2,8 @@
 function LandingPage() {
     return (
         <div>
-            <h1>Découvrez ChumConnect : Votre compagnon de sorties amicales !</h1>
+            <div>
+                <h1> Découvrez ChumConnect : Votre compagnon de sorties amicales !</h1>
             <p>
             Vous cherchez à élargir votre cercle d'amis et à vivre de nouvelles expériences ? 
             ChumConnect est là pour vous ! Notre plateforme vous permet de découvrir des activités 
@@ -11,6 +12,47 @@ function LandingPage() {
             Que vous souhaitiez proposer une sortie ou simplement rejoindre l'aventure d'un autre, 
             ChumConnect est votre passerelle vers des rencontres amicales inoubliables.
             </p>
+            <button>Rejoignez-nous !</button>
+            </div>
+            <div>
+                <h2>Pourquoi choisir ChumConnect ?</h2>
+                <div>
+                    <h3>Rencontres locales</h3>
+                    <p>Faites la connaissance de personnes partageant les mêmes 
+                        intérêts que vous, juste à côté de chez vous.
+                    </p>
+                </div>
+                <div>
+                    <h3>Diversité des activités</h3>
+                    <p>Des sorties en nature aux soirées cinéma, découvrez une variété 
+                        d'activités proposées par les membres.
+                    </p>
+                </div>
+                <div>
+                    <h3>Communauté conviviale</h3>
+                    <p>Rejoignez une communauté bienveillante et ouverte, prête à 
+                        accueillir de nouveaux membres.
+                    </p>
+                </div>
+                <div>
+                    <h3>Créez vos propres événements</h3>
+                    <p>Vous avez une passion ou un intérêt particulier que vous souhaitez partager ? 
+                        Créez votre propre événement et invitez d'autres à vous rejoindre.
+                    </p>
+                </div>
+                <div>
+                    <h3>Fonctionnalités de recherche avancées</h3>
+                    <p>Trouvez des événements et des personnes qui correspondent exactement à ce 
+                        que vous recherchez grâce aux fonctionnalités de recherche.
+                    </p>
+                </div>
+                <div>
+                    <h3>Des sorties pour tous les goûts</h3>
+                    <p>Que vous soyez un amateur de sport, un passionné de culture ou un amoureux 
+                        de la nature, vous trouverez votre bonheur parmi les sorties proposées.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
