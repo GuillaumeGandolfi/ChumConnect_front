fa8a2bd6875a353ece239a15699bb60856737a6c
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-09-18 22:13:38 +0200

    Started working on the landing page and set up TailwindCSS

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -2,7 +2,15 @@
 function LandingPage() {
     return (
         <div>
-            <h1>Landing Page</h1>
+            <h1>Découvrez ChumConnect : Votre compagnon de sorties amicales !</h1>
+            <p>
+            Vous cherchez à élargir votre cercle d'amis et à vivre de nouvelles expériences ? 
+            ChumConnect est là pour vous ! Notre plateforme vous permet de découvrir des activités 
+            passionnantes, des sorties culturelles ou même des escapades naturelles à proximité. 
+            Rejoignez une communauté bienveillante où tout est conçu pour créer des liens authentiques. 
+            Que vous souhaitiez proposer une sortie ou simplement rejoindre l'aventure d'un autre, 
+            ChumConnect est votre passerelle vers des rencontres amicales inoubliables.
+            </p>
         </div>
     );
 }
