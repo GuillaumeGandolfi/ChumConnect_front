1b5afa6129f6fb26a175b5ff2062a1b123b242dd
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-09-22 20:26:57 +0200

    Enhance signup page visuals using Tailwind CSS

--- src/components/views/SignupPage.jsx
+++ src/components/views/SignupPage.jsx
@@ -1,9 +1,58 @@
 function SignupPage() {
     return (
-        <div>
-            <h1>Signup Page</h1>
+        <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Créer un compte</h2>
+            </div>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form className="space-y-6" action="#" method="POST">
+                    <div>
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        Adresse Email
+                    </label>
+                    <div className="mt-1">
+                        <input id="email" name="email" type="email" autoComplete="email" required className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" />
+                    </div>
+                    </div>
+
+                    <div>
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                        Mot de passe
+                    </label>
+                    <div className="mt-1">
+                        <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" />
+                    </div>
+                    </div>
+
+                    <div>
+                    <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                        S'inscrire
+                    </button>
+                    </div>
+                </form>
+                
+                <div className="mt-6">
+                    <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-300"></div>
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="px-2 bg-white text-gray-500">
+                        Ou
+                        </span>
+                    </div>
+                    </div>
+
+                    <div className="mt-6 grid grid-cols-1 gap-3">
+                    {/* Ajoutez ici des boutons pour une connexion avec d'autres services si nécessaire */}
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default SignupPage;
