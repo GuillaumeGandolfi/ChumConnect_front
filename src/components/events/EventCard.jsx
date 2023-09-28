c045c4e58b22c28a369a627e98815569f5725dfd
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-09-28 20:39:38 +0200

    Enhanced the styling and customization of the event cards for a more distinctive and modern appearance.

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -1,9 +1,22 @@
+import { FaClock, FaMapMarkerAlt } from 'react-icons/fa';
+
 function EventCard({ event }) {
     return (
-        <div className="bg-white border p-4 rounded-md shadow-md m-2">
-            <h2 className="text-2xl font-semibold mb-2">{event.title}</h2>
-            <p className="text-gray-700">{event.description}</p>
-            {/* Ajouter ici d'autres détails de l'événement comme la date, le lieu, etc. */}
+        <div className="transform transition-transform duration-500 hover:scale-105 m-4">
+            <div className="bg-gray-800 text-white p-6 rounded-lg shadow-lg">
+                <h2 className="text-2xl font-semibold mb-4">{event.title}</h2>
+                <p className="text-gray-300 mb-4">{event.description}</p>
+                <div className="mt-2">
+                    <div className="flex items-center mb-2">
+                        <FaClock className="text-gray-400 mr-2" />
+                        <span className="text-gray-400 text-sm">{event.date} à {event.start_time}</span>
+                    </div>
+                    <div className="flex items-center">
+                        <FaMapMarkerAlt className="text-gray-400 mr-2" />
+                        <span className="text-gray-400 text-sm">{event.location}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
