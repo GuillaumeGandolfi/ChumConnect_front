c0ed64cf0e8814cdc7c11ac1bdc7cd2c4cfa2947
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-09-29 16:48:46 +0200

    Fixed the issue with user data not persisting after page refresh

--- src/components/views/ProfilePage.jsx
+++ src/components/views/ProfilePage.jsx
@@ -3,12 +3,16 @@ import { useUser } from '../../contexts/UserContext';
 
 function ProfilePage() {
     const { user } = useUser();
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
     console.log(user);
     
     return (
         <div className="container mx-auto py-8">
-            <h1 className="text-2xl font-semibold mb-4">Profil de {user && user.firstname}</h1>
-            <p>Email : {user && user.email}</p>
+            <h1 className="text-2xl font-semibold mb-4">Profil de {user.firstname}</h1>
+            <p>Email : {user.email}</p>
         </div>
     );
 }
